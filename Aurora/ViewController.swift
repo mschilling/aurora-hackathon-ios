6bb4693c7a198819c31eea7b4e44650333da9162
Julian van 't Veld <jveld@move4mobile.com>
2018-02-02 16:49:40 +0100

    Initial project commit

---
+++ Aurora/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aurora
+//
+//  Created by Julian van 't Veld on 02-02-18.
+//  Copyright © 2018 Julian van 't Veld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
